a86a89f8f7ad3c3eb4e75e70ff825ed2471efce4
antoniczapski <antoni.krzysztof.czapski@gmail.com>
2026-06-16 11:13:48 +0200

    Chapter 5

--- MarioDPO_v2/scripts/_t_accel.py
+++
@@ -1,5 +0,0 @@
-import accelerate
-print("accelerate", accelerate.__version__, flush=True)
-from accelerate import Accelerator
-a = Accelerator()
-print("accelerator ok, device:", a.device, flush=True)
--- MarioDPO_v2/scripts/_t_trainer.py
+++
@@ -1,4 +0,0 @@
-import torch
-print("torch", torch.__version__, flush=True)
-from transformers import Trainer
-print("trainer ok", flush=True)
